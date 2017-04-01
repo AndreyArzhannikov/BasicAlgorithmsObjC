d67d7678c88af17dca3a4924708e8dcca14bba68
Andrey Arzhannikov <andreya@handsome.is>
2017-04-01 15:21:51 +0600

    Initial Commit

---
+++ BasicAlgorithms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasicAlgorithms
+//
+//  Created by Andrey Arzhannikov on 01.04.17.
+//  Copyright © 2017 Handsome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BasicAlgorithms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasicAlgorithms
+//
+//  Created by Andrey Arzhannikov on 01.04.17.
+//  Copyright © 2017 Handsome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
